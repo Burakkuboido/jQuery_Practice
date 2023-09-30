b0e0bb6291f151508a0925f130153c7bce5ac040
Burakkuboido <Burakkuboido@hotmail.com>
2023-09-30 00:48:02 +0000

    Implented all code about jQuery. Next is to add flex to make it easier to read and navigate

--- scripts.js
+++ scripts.js
@@ -1,29 +1,17 @@
-// Getting Started With jQuery 
-
-/*
-$(document).ready(function(){
-
-    $("button").click(function(){  ### .ready is now deprecated. Use $(function(){ }) instead. 
-    });
-});
-*/
-
-
-
 // jQuery Events
 
-/*
+
 $(function(){
-    $("p").click(function(){  ### .click is now deprecated. Use .on instead.
-        console.log("You Clicked a Paragraph!")
+    $("#clicked").on("click", function(){ 
+        console.log("You Clicked me!")
     })
 });
-*/
+
 
 // dbclick, mouseenter, mouseleave,. mousedown, mouseup, hover. Hover uses TWO functions.
 
-/*
-$("p").on({
+
+$(".hover").on({
     mouseenter: function () {
         console.log("You entered.");
     },
@@ -31,39 +19,38 @@ $("p").on({
         console.log("You left.");
     }
 });
-*/
+
 
 //Run a Method ONCE.
-/*
+
 $(function(){
-    $("p").one({
+    $(".once").one({
         "click": function() {
             console.log("Clicked!");
-        },
-        "mouseover": function() {
-            console.log("Hovered!");
         }
     });
 })
-*/
 
 
 
 //jQuery Effects
 
 //hide, show, slideUp, slideDown, fadeIn, fadeOut, toggle, stop
-/*
-$("p").fadeOut(500);
-$("p").fadeIn(2000, function() {
+
+$("#fade").fadeOut(500);
+$("#fade").fadeIn(2000);
+
+$("#addclass").fadeOut(500);
+$("#addclass").fadeIn(2000, function() {
     $(this).addClass("blue");
 })
 
-$("p").on("click", function() {
-    $("p").stop();
+$("#fade").on("click", function() {
+    $("#fade").stop();
 })
 
 $("h3").hide(500).delay(1000).show(300);
-*/
+
 
 
 
@@ -72,94 +59,97 @@ $("h3").hide(500).delay(1000).show(300);
 // .animate ( properties [, duration ] [, easing ] [, complete ])
 
 // .animate( properties, options )
-/*
+
+// You will need jQuery UI to animate the background-color property. 
+
 $("#go").on("click", function() {
     $("#block").animate({
         opacity: .5,
         marginLeft: "+=50",
-        height: "400px"
+        backgroundColor: "red"
     }, 3000, "linear", function() {
         $(this).after("<div>Animation complete.</div>");
     });
 })
-*/
 
-/*
-$("#go").on("click", function() {
-    $("#block")
+
+
+$("#go2").on("click", function() {
+    $("#block2")
     .animate({width: "90%" }, 1000)
     .animate({fontSize: "40px" }, 1000)
     .animate({borderLeftWidth: "30px" }, 1000)
 })
-*/
+
 
 
 
 // jQuery: Getting and Setting Content and Attributes
 
-/*
+
 $("#btn").on("click", function () {
-    console.log($("#fcc").attr("href"));
+    console.log($("#google").attr("href"));
 });
 
 $("#btn2").on("click", function () {
     $("#test").text(function(i, origText) {
-        return "Old text: " + origText + " New text: freeCodeCamp is awesome! (index: " + i + ")"
+        return "Google is awesome! (index: " + i + ")"
     })
 });
 
 $("#btn3").on("click", function () {
     $("#name").val("Ben Green")
 });
-*/
+
 
 
 
 // jQuery: Adding and Removing Elements
 
-/*
-$(".target").prepend('<div class="child">Prepend</div>')
-$(".target").append('<div class="child">Append</div>', '<p> Hello!</p>')
-$(".target").before('<div class="sibling">Before</div>')
-$(".target").after('<div class="sibling">After</div>')
 
-$(".target").append('<b>LOTS</b>')
+$(".target").prepend('<div class="child">This has prepended to the beginning of the text</div>')
+$(".target").append('<div class="child">This has appended to the end of the div</div>')
+$(".target").before('<div class="sibling">This is added before the div is created.</div>')
+$(".target").after('<div class="sibling">This is added after the div is created.</div>')
 
-$("p").remove(".filter")
-*/
+$(".target").append('<b>Here we have appended a bold tag to the end of the .target element!</b>')
+
+$("#remove").on("click", function() {
+    $("p").remove(".filter")
+})
 
 
 
 // CSS and Classes
-/*
-$("button").on("click", function() {
-    $("p.big").css("fontSize", "100px");
-});
 
- $("button").on("click", function() {
-     $("p.big").css({
-         fontSize: "50px",
+
+ $("#changecss").on("click", function() {
+     $("p.changecss").css({
          color: "blue",
          backgroundColor: "yellow"
      });
  });
 
- $("button").on("click", function() {
-     $("h1").addClass("red")
-     $("p").removeClass("big")
+ $("#add").on("click", function() {
+     $(".forAdding").addClass("addedClass")
  })
 
- $("button").on("click", function() {
-     $("p").toggleClass("big")
+ $("#removes").on("click", function() {
+    $("p.forRemoving").removeClass("forRemoving")
+})
+
+ $("#toggle").on("click", function() {
+     $("p.forToggling").toggleClass("toggled")
  });
 
-$("button").on("click", function() {
-    console.log($("h2").hasClass("big"));
+$("#has").on("click", function() {
+    console.log($(".hasClass").hasClass("hasClass"));
 });
 
-*/
+
 
 // jQuery: Traversing the DOM
+
 
 //$("li").parent().css({"color" : "chocolate" , "border" : "2px solid chocolate"})
 //$("span").parents().css({"color" : "chocolate" , "border" : "2px solid chocolate"})
@@ -177,23 +167,10 @@ $("button").on("click", function() {
 
 //jQuery: AJAX = Asynchronous JavaScript and XML
 
-/*
-function loadDoc() {
-    $.post("https://learnwebcode.github.io/json-example/animals-1.json",
-    {
-        name: "Carlos",
-        city: "Teeden"
-    },
-    function(data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
-    });
-    $.get("https://learnwebcode.github.io/json-example/animals-1.json",
-    function(data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
-    });
 
-    /*
-    $("#demo").load("https://learnwebcode.github.io/json-example/animals-1.json",
+function loadDoc() {
+
+    $("#demo").load("https://gist.githubusercontent.com/readwalkerchang/2ab04719367da1778384762cc2befd23/raw/cb095bf75c39570b7a2734a8ab51c60c5b36eaa0/dark.json",
     function(responseTxt, statusTxt, xhr) {
         if(statusTxt == "success")
             alert("External content loaded");
@@ -201,10 +178,19 @@ function loadDoc() {
             alert("Error: " + xhr.status + ": " + xhr.statusText);
     })
 }
-*/
+
+$("#driver").on("click", function(event){
+    $.getJSON("https://gist.githubusercontent.com/readwalkerchang/2ab04719367da1778384762cc2befd23/raw/cb095bf75c39570b7a2734a8ab51c60c5b36eaa0/dark.json", function(data) {
+       $('#stage').html('<p>Name: ' + data.members[0].name + '</p>');
+       $('#stage').append('<p>Teen : ' + data.members[0].teen + '</p>');
+       $('#stage').append('<p>Adult  : ' + data.members[0].adult + '</p>');
+       $('#stage').append('<p>Description  : ' + data.members[0].description[0] + ". " + data.members[0].description[1] + ". " + data.members[0].description[2] + '</p>');
+    });
+ });
+
 
 //jQuery vs Vanilla JavaScript
-
+/*
 //Selecting an Element
 // the jQuery way...
 var $elem = $(".someclass");
@@ -241,7 +227,7 @@ requestAnimationFrame(() => elem.style.opacity = 1);
 // the jQuery way...
 var $elem = $(".someClass")
 $.ajax('https://some.url', {
-    success: (ddata) => { /* do stuff with the data */ }
+    success: (ddata) => { // do stuff with the data // }
 });
 
 //the vanilla JavaScript way...
@@ -275,4 +261,4 @@ elem.animate ([
         opacity: 1
     }
 ], 1000)
-
+*/
